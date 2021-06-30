1e6bf4d6963b4716204f65b404e74fcc4c9e7498
Teo <malehurtadoreyes@hotmail.com>
2021-06-30 10:23:39 -0500

    Update pagic.config.ts

--- pagic.config.ts
+++ pagic.config.ts
@@ -3,7 +3,7 @@
 export default {
   srcDir: '.',
   exclude: ['LICENSE'],
-  root: '/test-pagic',
+  root: '/test-pagic/',
 
   theme: 'docs',
   plugins: ['sidebar', 'prev_next'],
@@ -17,7 +17,7 @@ export default {
   nav: [
     {
       text: 'Docs',
-      link: '/pagic_template_docs/introduction/index.html',
+      link: '/test-pagic/introduction/index.html',
     },
     {
       text: 'Pagic',
@@ -25,7 +25,7 @@ export default {
     },
     {
       text: 'About',
-      link: '/pagic_template_docs/about/index.html',
+      link: '/test-pagic/about/index.html',
       align: 'right',
     },
   ],
